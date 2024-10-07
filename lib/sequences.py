583b1b4a9eedb9fdd5506da31dcc9ddcaa28bcf1
Dov Azogui <dov.azogui@gmail.com>
2024-10-06 21:03:24 -0400

    tg

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = [0, 1]
+    if length == 0:
+        print([]) 
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        for i in range(length - 2):
+            fibonacci_list.append(fibonacci_list[i] + fibonacci_list[i+1])
+        print(fibonacci_list)
+            
+print_fibonacci(100)
+            
+        
+        
+        
+        
+        
+        
+        
